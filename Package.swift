199717127838e20600de6a88964bfadc2ee20b2e
Isaac Halvorson <hello@hisaac.net>
2026-05-13 15:34:35 -0500

    add swift-subprocess

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
 	name: "swupdate",
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.1"),
+		.package(url: "https://github.com/swiftlang/swift-subprocess.git", from: "0.4.0"),
 	],
 	targets: [
 		.executableTarget(
 			name: "swupdate",
 			dependencies: [
 				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "subprocess", package: "swift-subprocess"),
 			]
 		),
 		.testTarget(
